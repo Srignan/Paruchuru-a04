494a028a8b9fd22f2696b0fc055294c6bd73b55f
Srignan <Srignan7@gmail.com>
2021-10-16 23:32:38 -0400

    43 Pseudocode

---
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -0,0 +1,34 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution43 {
+/*
+Call functions
+ */
+    }
+/*
+    public static boolean askJS(){
+        System.out.print("Do you want a folder for JavaScript? ");
+        String check = in.nextLine();
+        return check.equals("y") || check.equals("Y");
+    }
+
+    public static String askForName(){
+        System.out.print("Site name: ");
+        return in.nextLine();
+    }
+
+    public static String askForAuthor(){
+        System.out.print("Author: ");
+        return in.nextLine();
+    }
+
+    public static boolean askCSS(){
+        System.out.print("Do you want a folder for CSS? ");
+        String check = in.nextLine();
+        return check.equals("y") || check.equals("Y");
+    }
+}
+
+ */
---
+++ exercise43/src/main/java/baseline/WebsiteGenerator.java
@@ -0,0 +1,37 @@
+package baseline;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class WebsiteGenerator {
+    /*
+    State two strings siteName and author
+    Set them as files with public void setFiles(String siteName, String authorName)
+     */
+    /*
+    Public void makeDirectories(boolean js, boolean css
+    Create
+     String websiteStr = "./website"
+        Path websitePath = Paths.get(websiteStr)
+        try {
+            Files.createDirectory(websitePath)
+        } catch (IOException e) {
+            e.printStackTrace()
+        }
+     */
+    /*
+    Copy and paste same code from above but replace "./website/" with "./website/"+siteName
+    Do the same for JS and CSS
+     */
+    /*
+    public String makeHTML(){
+        //Make the file.
+        File html = new File("./website/"+siteName+"/index.html");
+        try{
+            //Output to file.
+            BufferedWriter bw = new BufferedWriter(new FileWriter(html));
+
+            bw.close
+     */
