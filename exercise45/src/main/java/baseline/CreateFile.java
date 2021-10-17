15b88ebc73eb70462c1d302d19bb2e40ab98d138
Srignan <Srignan7@gmail.com>
2021-10-17 18:39:00 -0400

    45 Pseudocode

---
+++ exercise45/src/main/java/baseline/CreateFile.java
@@ -0,0 +1,35 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Srignan Paruchuru
+ */
+
+package baseline;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+
+public class CreateFile {
+    /*
+    private static final Scanner in = new Scanner(System.in);
+
+    public void writeOut(String fileText){
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(askFilePath()));
+            bw.write(fileText);
+            bw.close();
+            System.out.println("Successfully wrote to file");
+        }catch(Exception ex){
+            System.out.println("Unsuccessful attempt");
+        }
+
+    }
+
+    public String askFilePath(){
+        System.out.print("Please enter the file path of the new file. (Remember to include file type '.txt') ");
+        return in.next();
+    }
+}
+
+     */
