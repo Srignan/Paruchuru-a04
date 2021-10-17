be482fc8e8cde5597616f2267d384bb6734a6853
Srignan <Srignan7@gmail.com>
2021-10-17 18:39:14 -0400

    45 Pseudocode

---
+++ exercise45/src/main/java/baseline/ReplaceWord.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Srignan Paruchuru
+ */
+
+package baseline;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/*
+Public class findAndReplace
+    private static final path filepath = path.get("exercise45_input.txt")
+
+    public String getFile(){
+        String fileText;
+            try{
+                fileText = Files.readString(filePath)
+                }catch ( Exception ex){
+                    fileText = "Cannot find file."
+                    }
+            return fileText;
+           }
+ */
+/*
+public String replace(String fileText){
+    fileText = fileText.replaceAll(utilize, use)
+    return fileText;
+    }
+ */
