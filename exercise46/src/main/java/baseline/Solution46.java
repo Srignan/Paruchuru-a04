28cc72bab2e27c89cef58206887da1d00dcfc67b
Srignan <Srignan7@gmail.com>
2021-10-17 21:18:19 -0400

    46 Complete

--- exercise46/src/main/java/baseline/Solution46.java
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -4,6 +4,7 @@
  */
 package baseline;
 
+
 public class Solution46 {
 /*
 public static void main (String[] args){
@@ -16,5 +17,14 @@ public static void main (String[] args){
             run.removeLineBreaks(
             run.parseFile()))))));
  */
-
+    public static void main(String[] args){
+        WordFrequency run = new WordFrequency();
+    System.out.print(
+       run.makeChart(
+       run.countWords(
+       run.removeBlanks(
+       run.makeList(
+       run.removeLineBreaks(
+       run.parseFile()))))));
+    }
 }
--- exercise46/src/main/java/baseline/WordFrequency.java
+++ exercise46/src/main/java/baseline/WordFrequency.java
@@ -48,4 +48,81 @@ public class WordFrequency {
                 }
             }
  */
+    private static final Path filePath = Paths.get("D:\\Java related\\Assignment04\\exercise46\\data\\exercise46_input.txt");
+
+    public String parseFile(){
+        String fileText;
+        try {
+            fileText = Files.readString(filePath);
+        }catch(Exception ex){
+            fileText = "Cannot find file.";
+        }
+
+        return fileText;
+    }
+    public String removeLineBreaks(String input){
+        //Replace all line breaks with spaces.
+        input = input.replace("\n", " ").replace("\r", "");
+        return input;
+    }
+    public List<String> makeList(String input){
+        //Return a string as a list of words.
+        return new ArrayList<>(Arrays.asList(input.split(" ")));
+    }
+    public List<String> removeBlanks(List<String> input){
+        //If the list contains blank instances it removes them and returns a new list without any blanks.
+        ArrayList<String> result = (ArrayList<String>) input;
+        for(int i=0; i<input.size(); i++){
+            if(result.get(i).isBlank()){
+                result.remove(i);
+            }
+        }
+        return result;
+    }
+    public Map<String,Integer> countWords(List<String> words){
+        Map<String,Integer> wordCount = new HashMap<>();
+        String compare;
+        boolean check;
+        //Loop through the list of words.
+        for (String word : words) {
+            check = false;
+            //Get the current word.
+            compare = word;
+            //If the word is in the ist of maps then increment the value of the map by one.
+            if ((wordCount.containsKey(compare))) {
+                check = true;
+                wordCount.put(compare, wordCount.get(compare) + 1);
+            }
+            //If the word is not in the list of maps add it with a value of '1'.
+            if (!check) {
+                wordCount.put(compare, 1);
+            }
+        }
+        return wordCount;
+    }
+    public String makeChart(Map<String,Integer> input){
+        StringBuilder result = new StringBuilder();
+        //Loop through and print the greatest amount of occurrences every time until there are no more values.
+        while(!input.isEmpty()){
+            Integer max = 0;
+            String maxKey = "";
+
+            //Find the entry with the largest value.
+            for (Map.Entry<String,Integer> entry : input.entrySet()) {
+                if(entry.getValue() > max){
+                    max = entry.getValue();
+                    maxKey = entry.getKey();
+                }
+            }
+            //Create a string with an equal amount of '*' to the value.
+
+            //Add a formatted line that include the word and its number of occurrences to the result.
+            result.append(String.format("%-15s", maxKey + ":")).append(String.format("%-10s", "*".repeat(max)
+                    //Add a formatted line that include the word and its number of occurrences to the result.
+            )).append("\n");
+            //Remove the word, so it does not get counted again.
+            input.remove(maxKey);
+        }
+        return result.toString();
+    }
 }
